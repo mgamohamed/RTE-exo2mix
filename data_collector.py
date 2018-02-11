cb813dc936f7d1a4bb3ae75e4b5b361c07b729a5
Adel <adel.sohbi@gmail.com>
2018-02-11 17:02:25 +0900

    minor corrections

--- data_collector.py
+++ data_collector.py
@@ -2,10 +2,11 @@
 # adelshb, github.com/adelshb
 # 2018/02/11
 
-from datetime import date, timedelta
+import datetime
 import pandas as pd
 import requests
 from lxml import html
+from datetime import date, timedelta
 
 def eco2mix_parser(url):
     
@@ -25,16 +26,18 @@ def eco2mix_parser(url):
     return data
 
 d1 = date(2012, 1, 1)  # start date
-d2 = date(2012, 1, 31)  # end date
+d2 = date(2018, 1, 31)  # end date
 delta = d2 - d1         # timedelta
 
-dates = [datetime.strptime(str(d1 + timedelta(days=i)), '%Y-%m-%d').strftime('%d/%m/%Y') for i in range(delta.days + 1)]
+dates = [datetime.datetime.strptime(str(d1 + timedelta(days=i)), '%Y-%m-%d').strftime('%d/%m/%Y') for i in range(delta.days + 1)]
 
 temp_url = "http://www.rte-france.com/getEco2MixXml.php?type=mix&&dateDeb=" + dates[0] +"&dateFin=" + dates[0] +"&mode=NORM"
 DATA = eco2mix_parser(temp_url)   
 
+print('In process...')
 for date in dates[1:]:
     temp_url = "http://www.rte-france.com/getEco2MixXml.php?type=mix&&dateDeb=" + date +"&dateFin=" + date +"&mode=NORM"
     DATA.append(eco2mix_parser(temp_url), ignore_index=True)
 
 DATA.to_csv('eco2mix_data.csv')
+print('Done!')
